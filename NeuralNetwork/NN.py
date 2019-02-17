1734c31513de25ed08153b60eda5f380fd363f58
Prasad Madhale <prasadchelsea33@gmail.com>
2019-02-17 04:10:28 -0500

    Wine classification using Keras, TF

--- NeuralNetwork/NN.py
+++ NeuralNetwork/NN.py
@@ -1,6 +1,8 @@
-import tensorflow as tf
 import pandas as pd
 import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from sklearn.model_selection import train_test_split
 
 
 def get_data(names):
@@ -9,57 +11,73 @@ def get_data(names):
     return df
 
 
-def test_train_split(data, test_percent=50):
-    test_size = int(len(data) * test_percent / 100)
-    test = data[:test_size]
-    train = data[test_size:]
+def split(df):
+    labels = df['label'].values
+    data = df.drop('label', axis=1).values
 
-    train_labels = train['label'].values
-    test_labels = test['label'].values
-
-    train = train.drop('label', axis=1).values
-    test = test.drop('label',axis=1).values
-
-    return train, test, train_labels, test_labels
+    return data, labels
 
 
-class NN:
-    def __int__(self, name, sess):
-        self.scope_name = name
-        self.sess = sess
+### TRAIN MODELS(Tensorflow)
+#
+#class NN:
+#    def __init__(self):
+#        self.input = tf.placeholder(shape=[None, 13], name='inputs', dtype=tf.float32)
+#        self.labels = tf.placeholder(shape=[None,3], name='labels', dtype=tf.float32)
+#        
+#        # prediction
+#        self.predict = tf.layers.dense(input, activation=tf.nn.sigmoid, units=3)
+#        
+#        # loss
+#        self.loss = tf.reduce_mean(tf.squared_difference(self.labels, self.predict))
+#        self.train_op = tf.train.AdamOptimizer(0.0001).minimize(self.loss)
 
-        with(tf.variable_scope('Neural_Network')):
-            self.input = tf.placeholder(shape = [None], name='inputs')
-            self.labels = tf.placeholder(shape = [None], name='labels')
-            self.out = self.build_network(self.input)
 
-        with(tf.variable_scope('loss')):
-            loss = tf.losses.mean_squared_error(self.labels, self.out)
-            self.train_op = tf.train.AdamOptimizer(0.001).minimize(loss)
-
-    def build_network(self,inputs):
-        l1 = tf.layers.dense(inputs, activation = tf.sigmoid, units=32)
-        l2 = tf.layers.dense(l1, activation= tf.sigmoid, unit=16)
-        out = tf.layers.dense(l2, activation=tf.sigmoid, unit=1)
-        return out
-
-    def trainable_vars(self):
-        return tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=self.scope_name)
-
-    def train(self, inputs, true_labels, sess):
-        return self.sess.run(self.out, feed_dict={self.input:inputs,
-                                                  self.labels:true_labels})
-
+## DATA PROCESSING
 
 column_names = ['label', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'f12', 'f13']
 dataframe = get_data(column_names)
-train_data, test_data, train_label, test_label = test_train_split(dataframe)
 
-# with tf.Session() as sess:
+# shuffle data
+dataframe = dataframe.sample(frac = 1)
+
+data, label = split(dataframe)
+
+# normalize
+data = keras.utils.normalize(data, axis=1, order=2)
+
+train_data, test_data, train_label, test_label = train_test_split(data, label, test_size = 0.5)
+
+#train_label = pd.get_dummies(train_label, dtype = np.float32).values
+#test_label = pd.get_dummies(test_label, dtype = np.float32).values
 #
+
+### TRAIN MODEL (Tensorflow)
 #
-#     tf.run(tf.global_variables_initializer)
-#     nn = NN('NeuralNet', sess)
+#init = tf.global_variables_initializer()
+#
+#with tf.Session() as sess:
+#    sess.run(init)
+#    
+#    mlp = NN()
+#    sess.run(mlp.predict, feed_dict={mlp.input: train_data,
+#                                     mlp.labels: train_label})
 
-    # predict = train(train_data, labels,sess)
 
+    
+## TRAIN MODEL (KERAS)
+
+test_label = keras.utils.to_categorical(test_label-1,3)
+train_label = keras.utils.to_categorical(train_label-1,3)
+
+## KERAS
+model = keras.Sequential()
+model.add(keras.layers.Dense(32, input_dim=13, 
+                             kernel_regularizer=keras.regularizers.l2(0.),
+                             kernel_initializer=keras.initializers.glorot_normal(seed=0),
+                             activation='relu'))
+model.add(keras.layers.Dense(3, activation='softmax'))
+
+optimize = keras.optimizers.Adam(lr=0.001)
+model.compile(loss='categorical_crossentropy', optimizer=optimize, metrics=['accuracy'])
+model.fit(train_data, train_label, batch_size=25,epochs=5000, validation_data = (test_data, test_label))
