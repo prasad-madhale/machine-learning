fbe65381ed941a973d736e1876807b5af5b31ff3
Prasad Madhale <prasadchelsea33@gmail.com>
2019-04-16 19:50:23 -0400

    Add Adaboost on Polluted Data

--- Adaboost_with_bad_features/adaboost_polluted.py
+++ Adaboost_with_bad_features/adaboost_polluted.py
@@ -1,15 +1,16 @@
 import numpy as np
 import pandas as pd
 from sklearn import preprocessing
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, auc, roc_curve
+from sklearn.metrics import accuracy_score, roc_curve, auc
+import matplotlib.pyplot as plt
+import random
 
 
 class AdaBoost:
 
     def __init__(self):
         self.train_data, self.train_labels, self.test_data, self.test_labels = AdaBoost.get_data()
-        self.data = AdaBoost.normalize(self.data.astype(np.float))
+        self.train_data, self.test_data = AdaBoost.normalize(self.train_data, self.test_data)
 
     @staticmethod
     def get_data():
@@ -18,19 +19,50 @@ class AdaBoost:
         train_data = pd.read_csv('./data/spam_polluted/train_feature.txt', sep=' ')
         train_label = pd.read_csv('./data/spam_polluted/train_label.txt', sep='\n')
 
+        # flatten labels
+        test_label = test_label.values.flatten()
+        train_label = train_label.values.flatten()
+
+        # change all 0s to -1s
+        test_label[test_label == 0] = -1
+        train_label[train_label == 0] = -1
+
         return train_data, train_label, test_data, test_label
 
     @staticmethod
-    def normalize(dataset):
-        return preprocessing.minmax_scale(dataset, feature_range=(0, 1))
+    def normalize(train_data, test_data):
+        # combine data
+        full_data = np.concatenate([train_data, test_data])
+
+        test_data_size = len(train_data)
+
+        # normalize data
+        full_data = preprocessing.minmax_scale(full_data, feature_range=(0, 1))
+
+        train_data = full_data[:test_data_size]
+        test_data = full_data[test_data_size:]
+
+        return train_data, test_data
 
     def fit(self, num_weak_learners=100):
-        train_data, test_data, train_label, test_label = train_test_split(self.data, self.labels, test_size=0.25)
-
         # train for the training data
-        models, summary, features = AdaBoost.train(train_data, train_label, test_data, test_label, num_weak_learners)
+        models, summary = AdaBoost.train(self.train_data, self.train_labels, self.test_data, self.test_labels,
+                                         num_weak_learners)
 
-        return features
+        # plot round errors
+        AdaBoost.plot_round_error(summary[:, 0])
+
+        # plot train/test errors
+        AdaBoost.plot_train_test_error(summary[:, 1], summary[:, 2])
+
+        # plot test AUC scores
+        AdaBoost.plot_auc(summary[:, 3])
+
+        # get predictions for test data using final models
+        test_predictions = WeakLearner.get_prediction(models, self.test_data)
+
+        # plot roc
+        AdaBoost.plot_roc(self.test_labels, test_predictions)
 
     @staticmethod
     def train(train_data, train_label, test_data, test_label, num_weak_learners):
@@ -42,12 +74,10 @@ class AdaBoost:
         wts = np.full(train_data.shape[0], (1 / train_data.shape[0]))
 
         # get all unique thresholds in sorted order
-        thresholds = WeakLearner.get_optimal_thresholds(train_data)
+        thresholds = WeakLearner.get_thresholds(train_data)
 
-        # pick midpoints from the thresholds
+        # # pick midpoints from the thresholds
         thresholds = WeakLearner.get_midpoints(thresholds)
-
-        features = []
 
         for ep in range(num_weak_learners):
             # get predictor using different decision stump selection techniques
@@ -57,7 +87,7 @@ class AdaBoost:
             error = model.error
 
             # calculate alpha for this model
-            model.alpha = 0.5 * np.log((1 - error) / (error + 1e-10))
+            model.alpha = 0.5 * np.log((1 - error) / (error + 1e-15))
 
             preds = np.ones(np.shape(train_label))
             preds[train_data[:, model.feature] <= model.threshold] = -1
@@ -68,28 +98,73 @@ class AdaBoost:
 
             models.append(model)
 
-            features.append(model.feature)
-
             train_error, _ = AdaBoost.get_error(models, train_data, train_label)
-            test_error, auc_score = AdaBoost.get_error(models, test_data, test_label)
+            test_error, test_acc = AdaBoost.get_error(models, test_data, test_label)
 
             # add to summary all the values needed for plotting
-            summary[ep] = [model.error, train_error, test_error, auc_score]
+            summary[ep] = [model.error, train_error, test_error, test_acc]
 
-            print('Round: {}  |  Feature: {}  |  Threshold: {}  |  Round_error: {}  |  Train_error: {}  |  '
-                  'Test_error: {}  |  AUC: {}'.format(ep+1, model.feature, model.threshold, model.error, train_error,
-                                                      test_error, auc_score), file=f)
+            print('Round: {}  |  Feature: {}  |  Threshold: {}  |  Round_error: {}  |  Train_error: {}  |   '
+                  'Test_error: {}  |  Test Accuracy: {}'.format(ep, model.feature, model.threshold, model.error,
+                                                                train_error, test_error, test_acc), file=f)
 
-        return models, summary, features
+        return models, summary
 
     @staticmethod
     def get_error(models, data, label):
         preds = WeakLearner.get_prediction(models, data)
         acc = accuracy_score(label, preds)
+        return 1 - acc, acc
 
-        fpr, tpr, thresholds = roc_curve(label, preds)
-        auc_score = auc(fpr, tpr)
-        return 1 - acc, auc_score
+    @staticmethod
+    def plot_round_error(round_errors):
+        plt.xlabel('Iteration Step')
+        plt.ylabel('Round Error')
+        plt.title('Round Error')
+        plt.plot(round_errors)
+        plt.savefig('./plots/round_error.png')
+        plt.close()
+
+    @staticmethod
+    def plot_train_test_error(train_error, test_error):
+        num_itrs = train_error.shape[0]
+        itrs = np.arange(num_itrs)
+
+        plt.xlabel('Iteration Step')
+        plt.ylabel('Train/Test Error (Red/Blue)')
+        plt.title('Train/Test Error (Red/Blue)')
+
+        plt.plot(itrs, train_error, 'r-', label='Train Error')
+        plt.plot(itrs, test_error, 'b-', label='Test Error')
+
+        # add legend
+        plt.legend(loc='upper right')
+
+        plt.savefig('./plots/train_test_error.png')
+        plt.close()
+
+    @staticmethod
+    def plot_auc(aucs):
+        plt.xlabel('Iteration Step')
+        plt.ylabel('AUC')
+        plt.title('AUC')
+        plt.plot(aucs)
+        plt.savefig('./plots/auc.png')
+        plt.close()
+
+    @staticmethod
+    def plot_roc(truth, preds):
+        fprs, tprs, _ = roc_curve(truth, preds)
+
+        plt.title('ROC')
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+
+        plt.plot(fprs, tprs, label='AUC: {}'.format(auc(fprs, tprs)))
+        plt.legend(loc='lower right')
+
+        plt.savefig('./plots/roc.png')
+        plt.close()
 
 
 class Node:
@@ -105,12 +180,13 @@ class Node:
 class WeakLearner:
 
     @staticmethod
-    def get_optimal_thresholds(data):
+    def get_thresholds(data):
         thresholds = []
 
         for i in range(data.shape[1]):
-            uniques = np.unique(data[:, i])
-            thresholds.append(uniques)
+            uniqs = np.unique(data[:, i])
+            uniqs = np.sort(uniqs)
+            thresholds.append(uniqs)
 
         return np.array(thresholds)
 
@@ -126,7 +202,6 @@ class WeakLearner:
 
     @staticmethod
     def get_tree_predictor(dataset, thresholds, labels, wts):
-
         max_diff = -float('inf')
 
         # check each threshold, feature pair to find the best one
@@ -178,9 +253,12 @@ class WeakLearner:
         return prediction
 
 
+# set seed for random operations
+np.random.seed(2)
+random.seed(2)
+
 with open('./logs/out_polluted.txt', 'w') as f:
     ada_boost = AdaBoost()
-    print(ada_boost.train_data.shape, ada_boost.train_labels.shape, ada_boost.test_data.shape,
-          ada_boost.test_labels.shape)
+    ada_boost.fit(num_weak_learners=15)
 
 print('Done Training!')
